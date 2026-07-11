fc5b8a0a1f218e4dfd6144471d57dce35475eeb8
Sveta <merkulsy@gmail.com>
2026-07-11 17:48:17 +0300

    add tokens

---
+++ vk_test_access_token.py
@@ -0,0 +1,20 @@
+import requests
+from secrets import access_token, user_id
+
+class VK:
+
+    def __init__(self, access_token, user_id, version='5.199'):
+        self.token = access_token
+        self.id = user_id
+        self.version = version
+        self.params = {'access_token': self.token, 'v': self.version}
+
+    def users_info(self):
+        url = 'https://api.vk.com/method/users.get'
+        params = {'user_ids': self.id}
+        response = requests.get(url, params={**self.params, **params})
+        return response.json()
+
+vk = VK(access_token, user_id)
+
+print(vk.users_info())
