c217650f31500d6fa0e88d0285b7ddae058048e6
Sveta <merkulsy@gmail.com>
2026-07-12 16:22:29 +0300

    add cщonfig.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Словарь с конфигурацией подключения к базе данных PostgreSQL.
+# Значения берутся из переменных окружения, а если они не заданы, то берутся по умолчанию
+DB_CONFIG = {
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'database': os.getenv('DB_NAME', 'vk_inder_db'),
+    'user': os.getenv('DB_USER', 'postgres'),
+    'password': os.getenv('DB_PASSWORD', 'postgres'),
+}
+
+# Версия API ВКонтакте, актуальная на момент написания кода
+VK_VERSION = '5.199'
