9b7e5de1159920e2bc20e8633918da9c267be2ef
Sveta <merkulsy@gmail.com>
2026-07-12 17:59:27 +0300

    add vk_test_group_token.py

---
+++ vk_test_group_token.py
@@ -0,0 +1,16 @@
+import requests
+from secrets import group_token
+
+class VKGroup:
+    def __init__(self, group_token, version='5.199'):
+        self.token = group_token
+        self.version = version
+        self.params = {'access_token': self.token, 'v': self.version}
+
+    def get_info(self):
+        url = 'https://api.vk.com/method/groups.getById'
+        response = requests.get(url, params=self.params)
+        return response.json()
+
+vk_group = VKGroup(group_token)
+print(vk_group.get_info())
